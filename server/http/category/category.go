d98ce31487e1a403aef9993ca605a795b2cae1cf
Victor Schumacher <victor.schumacher@zup.com.br>
2020-04-11 16:26:54 -0300

    Added category controllers

---
+++ server/http/category/category.go
@@ -0,0 +1,67 @@
+package category
+
+import (
+	"encoding/json"
+	"github/kaduartur/jams-manager/server"
+	"net/http"
+	"strings"
+)
+
+var categories = server.Categories{}
+
+func Handle() http.HandlerFunc {
+
+	return func(w http.ResponseWriter, r *http.Request) {
+
+		switch r.Method {
+
+		case http.MethodGet:
+			processGet(w)
+		case http.MethodPost:
+			processPost(w, r)
+		case http.MethodDelete:
+			processDelete(w,r)
+		default:
+			http.NotFound(w, r)
+		}
+
+	}
+}
+
+func processPost(w http.ResponseWriter, r *http.Request) {
+
+	category := server.Category{}
+
+	defer r.Body.Close()
+
+	err := json.NewDecoder(r.Body).Decode(&category)
+	if err != nil {
+		w.Write([]byte("Error while processing data"))
+	}
+
+	categories = append(categories, category)
+}
+
+func processGet(w http.ResponseWriter) {
+
+	w.Header().Add("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(categories)
+}
+
+func serviceFromPath(path string) string {
+	return strings.Replace(path, "/categories/", "", 1)
+
+}
+
+func processDelete(w http.ResponseWriter, r *http.Request) {
+	id := serviceFromPath(r.URL.Path)
+
+	for i := range categories {
+		if categories[i].Id == id {
+			categories = append(categories[:i], categories[i+1:]...)
+			break
+		}
+
+	}
+}
+
